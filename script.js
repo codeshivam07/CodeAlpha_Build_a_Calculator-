5f2cf0d8d811980beeceda70a1da14025b1ae06a
codeshivam07 <shivam1149og@gmail.com>
2025-08-08 15:04:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculate(operator) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Please enter valid numbers.";
+  } else {
+    switch(operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num2 !== 0 ? (num1 / num2) : "Cannot divide by zero!";
+        break;
+      default:
+        result = "Invalid operator";
+    }
+  }
+  function refreshCalculator() {
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+  document.getElementById("result").innerText = "Result:";
+}
+
+
+  document.getElementById("result").innerText = "Result: " + result;
+}
